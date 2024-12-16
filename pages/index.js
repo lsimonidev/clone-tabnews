7fdbe858a51b7a6aade0a2016243c5aa33f186b4
Laura Simoni <laura.simoni@hostelworld.com>
2024-12-16 14:36:38 +0000

    Configurar o EditorConfig

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Hello(){
-    return <h1>Se você ama o Gustavo, faz uma dancinha</h1>
+function Hello() {
+  return <h1>Se você ama o Gustavo, faz uma dancinha</h1>;
 }
 
 export default Hello;
