96476854fe4369e2bcecdc3516264df75de96622
Laura Simoni <laura.simoni@gmail.com>
2025-01-06 13:03:25 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(req, res){
-  res.status(200).json({chave: 'valor'});
+import database from '../../../../infra/database.js';
+
+async function status(req, res){
+  const result = await database.query('SELECT 1+1;')
+  res.status(200).json({chave: 'valor'}); 
 }
 
-export default status;
+export default status; 
