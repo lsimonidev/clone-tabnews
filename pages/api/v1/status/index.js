5c5b176fce259f280716d49602cc9fcf60f84c5d
Laura Simoni <laura.simoni@gmail.com>
2025-01-09 10:53:37 +0000

    add `.prettierignore` and linter

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from '../../../../infra/database.js';
+import database from "../../../../infra/database.js";
 
-async function status(req, res){
-  const result = await database.query('SELECT 1+1;')
-  res.status(200).json({chave: 'valor'}); 
+async function status(req, res) {
+  const result = await database.query("SELECT 1+1;");
+  res.status(200).json({ chave: "valor" });
 }
 
-export default status; 
+export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,6 @@
-describe('API status - v1', () => {
-  test('GET to /api/v1/status should return 200', async () => {
+describe("API status - v1", () => {
+  test("GET to /api/v1/status should return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
     expect(response.status).toBe(200);
   });
-})
+});
