894ab4fc19cb033255a6db7d21bc296bb95ebbd2
Laura Simoni <laura.simoni@gmail.com>
2025-02-06 11:16:14 +0000

    add `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -9,14 +9,13 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     ssl: getSSLValues(),
   });
-  
-  
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error('Unable to retrieve database query:' + error);
+    console.error("Unable to retrieve database query:" + error);
     throw error;
   } finally {
     await client.end();
@@ -27,11 +26,11 @@ export default {
   query: query,
 };
 
-function getSSLValues(){
-  if(process.env.POSTGRES_CA){
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
     return {
       ca: process.env.POSTGRES_CA,
-    }
+    };
   }
-  return process.NODE_ENV === 'development'?false:true
-};
+  return process.env.NODE_ENV === "production";
+}
