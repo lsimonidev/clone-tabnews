d52da9c4b5f710926afa337fc2500ec299d6e64e
Laura Simoni <laura.simoni@gmail.com>
2025-02-07 10:41:12 +0000

    refactor migrations endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,29 +1,38 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database";
 
 export default async function migrations(req, res) {
+
+  let dbClient;
+
   if (req.method === "GET") {
+    dbClient = await database.getNewClient();
     const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+      dbClient,
       dryRun: true,
       dir: join("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
     });
+    dbClient.end();
     return res.status(200).json(migrations);
   }
+
   if (req.method === "POST") {
+    dbClient = await database.getNewClient();
     const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+      dbClient,
       dryRun: false,
       dir: join("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
     });
+    dbClient.end();
     return res.status(200).json(migrations);
   }
-
+  
   return res.status(405).end;
 }
