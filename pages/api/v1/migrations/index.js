fb9ba378de3a518c3edd25971975a9cb04e4c2a2
Laura Simoni <laura.simoni@hostelworld.com>
2025-03-13 12:20:42 +0000

    fix `/migration` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,36 +3,41 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(req, res) {
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(req.method)) {
+    return res.status(405).json({
+      error: `Method "${req.method}" not allowed`,
+    });
+  }
 
   let dbClient;
 
-  if (req.method === "GET") {
+  try {
     dbClient = await database.getNewClient();
-    const migrations = await migrationRunner({
+    const defaultMigrationsOptions = {
       dbClient,
-      dryRun: true,
       dir: join("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
-    });
-    dbClient.end();
-    return res.status(200).json(migrations);
-  }
+    };
 
-  if (req.method === "POST") {
-    dbClient = await database.getNewClient();
-    const migrations = await migrationRunner({
-      dbClient,
-      dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
+    if (req.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+      return res.status(200).json(pendingMigrations);
+    }
+
+    if (req.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: false,
+      });
+      return res.status(200).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
     dbClient.end();
-    return res.status(200).json(migrations);
   }
-  
-  return res.status(405).end;
 }
